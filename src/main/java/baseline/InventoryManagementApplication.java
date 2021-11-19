b4e9f68d014521200310cb7acbf7a535b951fd0f
Fed-Abreu <arielsey@hotmail.com>
2021-11-19 18:22:07 -0500

    first commit

---
+++ src/main/java/baseline/InventoryManagementApplication.java
@@ -0,0 +1,27 @@
+package baseline;
+/*
+ *  UCF COP3330 Fall 2021 Application Assignment 2 Solution
+ *  Copyright 2021 Federico Abreu Seymour
+ */
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.util.Objects;
+public class InventoryManagementApplication extends Application{
+    @Override
+    public void start(Stage stage) throws IOException {
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("InventoryManager.fxml")));
+
+        Scene scene = new Scene(root);
+        stage.setTitle("Inventory");
+        stage.setScene(scene);
+        stage.show();
+    }
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
