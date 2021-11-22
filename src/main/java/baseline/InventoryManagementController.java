2b676f0276e42a85e8faa814eccb5e146a831fde
Fed-Abreu <arielsey@hotmail.com>
2021-11-21 23:05:06 -0500

    finished part1

--- src/main/java/baseline/InventoryManagementController.java
+++ src/main/java/baseline/InventoryManagementController.java
@@ -4,6 +4,7 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+
 import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.input.MouseEvent;
@@ -15,8 +16,6 @@ import javafx.stage.Stage;
 import java.io.*;
 import java.net.URL;
 import java.util.ResourceBundle;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 public class InventoryManagementController {
 
@@ -62,10 +61,39 @@ public class InventoryManagementController {
     private final ObservableList<Product> tableData = FXCollections.observableArrayList();
 
 
+    public void initialize(URL location, ResourceBundle resources) {
+        fileChooser.setInitialDirectory(new File("C:"));
+    }
+
+
     @FXML
     void addProduct(MouseEvent event) {
         //if serialNumber is empty
-        if(priceBox.getText().matches("[a-zA-Z][-][0-9a-zA-Z]{3}[-][a-zA-Z0-9]{3}[-][a-zA-Z0-9]{3}$")){
+        if (serialBox.getText().matches("")) {
+            //Display error message
+            Alert productAlert = new Alert(Alert.AlertType.ERROR, "Serial Number cannot be empty!");
+            productAlert.show();
+        }
+
+        //else if name is empty
+        else if (nameBox.getLength() <= 1 || nameBox.getLength() >= 257) {
+            //Display Error message
+            Alert productAlert = new Alert(Alert.AlertType.ERROR, "Product Name must be 2 or more characters and less than 256!");
+            productAlert.show();
+        }
+
+        //else if price is empty
+        else if (priceBox.getText().matches("[0]")) {
+            //Display error message
+            Alert productAlert = new Alert(Alert.AlertType.ERROR, "Can not be empty!");
+            productAlert.show();
+        } else if (!priceBox.getText().matches("^(0|[1-9][0-9]*)$")) {
+            Alert productAlert = new Alert(Alert.AlertType.ERROR, "Must enter a Number!");
+            productAlert.show();
+        }
+
+        //else make new products add data to products
+        else {
             //Set tablebview items from data
             Product product = new Product(nameBox.getText(), serialBox.getText(), Double.parseDouble(priceBox.getText()));
             tableData.add(product);
@@ -76,36 +104,7 @@ public class InventoryManagementController {
             serialBox.setText("");
             //Set text of price
             priceBox.setText("");
-        }
-        else if(serialBox.getText().matches(""))
-        {
-            //Display error message
-            Alert productAlert = new Alert(Alert.AlertType.ERROR, "Serial Number cannot be empty!");
-            productAlert.show();
-        }
 
-        //else if name is empty
-        else if(nameBox.getText().matches(""))
-        {
-            //Display Error message
-            Alert productAlert = new Alert(Alert.AlertType.ERROR, "Product Name cannot be empty!");
-            productAlert.show();
-        }
-
-        //else if price is empty
-        else if(priceBox.getText().matches(""))
-        {
-            //Display error message
-            Alert productAlert = new Alert(Alert.AlertType.ERROR, "Invalid price!");
-            productAlert.show();
-        }
-
-
-        //else make new products add data to products
-        else
-        {
-            Alert productAlert = new Alert(Alert.AlertType.ERROR, "Invalid Format");
-            productAlert.show();
         }
     }
 
@@ -129,34 +128,34 @@ public class InventoryManagementController {
 
         }
     }
+
     @FXML
     public void loadData(File file, String fileExtension) throws IOException {
         //if file extension is not a txt
-            if(!fileExtension.matches("txt")) {
-                //Show file type and error message
-                System.out.println(fileExtension);
-                Alert fileAlert = new Alert(Alert.AlertType.ERROR, "Invalid File Type! Only .txt files allowed");
-                fileAlert.show();
-            }
-                //else buffer read file and while String line is not empty
-        else{
-                BufferedReader reader  = new BufferedReader(new FileReader(file));
-                String line;
-                while((line = reader.readLine()) != null){
-                    //Parse the products
-                    String[] data = line.split("\t");
-                    Product product = new Product(data[1], data[0],Double.parseDouble(data[2]));
+        if (!fileExtension.matches("txt")) {
+            //Show file type and error message
+            System.out.println(fileExtension);
+            Alert fileAlert = new Alert(Alert.AlertType.ERROR, "Invalid File Type! Only .txt files allowed");
+            fileAlert.show();
+        }
+        //else buffer read file and while String line is not empty
+        else {
+            BufferedReader reader = new BufferedReader(new FileReader(file));
+            String line;
+            while ((line = reader.readLine()) != null) {
+                //Parse the products
+                String[] data = line.split("\t");
+                Product product = new Product(data[1], data[0], Double.parseDouble(data[2]));
 
-                    //THen add to table
-                    tableData.add(product);
-                }
-                //Set Data to table
-                tableView.setItems(tableData);
-                reader.close();
+                //THen add to table
+                tableData.add(product);
             }
+            //Set Data to table
+            tableView.setItems(tableData);
+            reader.close();
+        }
 
-            }
-
+    }
 
 
     @FXML
@@ -187,7 +186,8 @@ public class InventoryManagementController {
         Label lblUpdate = new Label("Product Price: ");
         TextField txtBox = new TextField();
         Button saveBtn = new Button("Save");
-        saveBtn.setOnMouseClicked(event->{});
+        saveBtn.setOnMouseClicked(event -> {
+        });
         //Add elements to box
         vbox.getChildren().addAll(lblUpdate, txtBox, saveBtn);
         //create prompt
@@ -196,28 +196,39 @@ public class InventoryManagementController {
         updatePrompt.show();
     }
 
-
+    @FXML
     public void onSave(ActionEvent actionEvent) {
-        //Use FileChooser to save
-        File savesFile = fileChooser.showSaveDialog(null);
-        if (savesFile != null) {
-            try {
-                //Use PrintWriter to write to file
-                PrintWriter pw = new PrintWriter(savesFile);
-                //Loop for each task in list
-                for (Product t: tableData){
-                    //Write current task date to file and description
-                    pw.write(t.getSerialNumber() + '\n');
-                    pw.write(t.getName() + '\n');
-                    pw.write((int) (t.getPrice() + '\n'));
-                }
-                pw.close();
-                //Catch Exceptions
-            } catch (IOException ex) {
 
-                Logger.getLogger(InventoryManagementApplication.class.getName()).log(Level.SEVERE, null, ex);
-            }
+        FileChooser fileChooser = new FileChooser();
+        //Set title
+        fileChooser.setTitle("Save Dialog");
+        //Open Dialog window
+
+        fileChooser.getExtensionFilters().addAll(new FileChooser.ExtensionFilter("text file", "*.txt"),
+                new FileChooser.ExtensionFilter("HTML", "*.html"), new FileChooser.ExtensionFilter("JSON", "*.json"),
+                new FileChooser.ExtensionFilter("tab-separated value", "*.tsv"));
+        File file = fileChooser.showSaveDialog(filterArea.getScene().getWindow());
+        if (file != null) {
+            try {
+
+                fileChooser.setInitialDirectory(file.getParentFile());
+
+                PrintWriter printWriter = new PrintWriter(file);
+                for(Product p :tableData) {
+                    printWriter.write(p.getSerialNumber());
+                    printWriter.write(p.getName() + '\n');
+                    printWriter.write(String.valueOf(p.getPrice() + '\n'));
+
                 }
+                printWriter.close();
+
+
+
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+        }
+
     }
 }
 
